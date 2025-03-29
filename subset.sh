c62877cd1a5bd99d6dfda676fb349e3e94cf92c3
Alex Russell <slightlyoff@gmail.com>
2025-03-29 16:14:25 -0700

    update font w/ RSS icon

--- subset.sh
+++ subset.sh
@@ -4,13 +4,16 @@ MERGED="./fontawesome/merged.ttf"
 COMBINED="./combined.woff2"
 
 # TODO: to add/update an icon, you first have to clobber $MERGED. Automate.
-if [ ! -f $MERGED ]
-then
-  fonttools merge --output-file=$MERGED ./fontawesome/*.ttf
-fi
+# if [ ! -f $MERGED ]
+# then
+#  fonttools merge --output-file=$MERGED ./fontawesome/*.ttf
+# fi
 
-# Share, Tweet, Link, Toot, LI Promote, Skeet
-pyftsubset $MERGED --unicodes="U+F14D,U+F099,U+F0C1,U+F4F6,U+F08C,U+E671" \
+# Merge every time to avoid stale update bugs
+fonttools merge --output-file=$MERGED ./fontawesome/*.ttf
+
+# Share, Tweet, Link, Toot, LI Promote, Skeet, RSS
+pyftsubset $MERGED --unicodes="U+F14D,U+F099,U+F0C1,U+F4F6,U+F08C,U+E671,U+F09E" \
   --flavor=woff2 --output-file=$COMBINED
 
 base64 $COMBINED \
