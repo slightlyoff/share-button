ad5976dc114d2aa0eb5cbec8d29bd462977e106a
Alex Russell <slightlyoff@gmail.com>
2025-03-29 14:24:33 -0700

    update w/ bsky and li icons

--- subset.sh
+++ subset.sh
@@ -3,13 +3,14 @@
 MERGED="./fontawesome/merged.ttf"
 COMBINED="./combined.woff2"
 
+# TODO: to add/update an icon, you first have to clobber $MERGED. Automate.
 if [ ! -f $MERGED ]
 then
   fonttools merge --output-file=$MERGED ./fontawesome/*.ttf
 fi
 
-# Share, Tweet, Link, Toot
-pyftsubset $MERGED --unicodes="U+F14D,U+F099,U+F0C1,U+F4F6" \
+# Share, Tweet, Link, Toot, LI Promote, Skeet
+pyftsubset $MERGED --unicodes="U+F14D,U+F099,U+F0C1,U+F4F6,U+F08C,U+E671" \
   --flavor=woff2 --output-file=$COMBINED
 
 base64 $COMBINED \
